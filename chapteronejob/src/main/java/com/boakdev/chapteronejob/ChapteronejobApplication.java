bf24b425df25d341d5de05b3dce0f87c580d6564
Bruno-Carvalho <brunooakdev@gmail.com>
2021-01-17 11:29:38 -0300

    First commit - Project created

---
+++ chapteronejob/src/main/java/com/boakdev/chapteronejob/ChapteronejobApplication.java
@@ -0,0 +1,13 @@
+package com.boakdev.chapteronejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChapteronejobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChapteronejobApplication.class, args);
+	}
+
+}
