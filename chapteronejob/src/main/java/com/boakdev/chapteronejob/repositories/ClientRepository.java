fece999b694c5057b3b24b698ed34196a8f976db
Bruno-Carvalho <brunooakdev@gmail.com>
2021-01-17 12:00:14 -0300

    include annotation @Repository in Client repository

--- chapteronejob/src/main/java/com/boakdev/chapteronejob/repositories/ClientRepository.java
+++ chapteronejob/src/main/java/com/boakdev/chapteronejob/repositories/ClientRepository.java
@@ -1,9 +1,11 @@
 package com.boakdev.chapteronejob.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.boakdev.chapteronejob.entities.Client;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
