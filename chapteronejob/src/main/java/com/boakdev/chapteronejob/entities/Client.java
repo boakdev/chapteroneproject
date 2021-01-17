1ae2f1b2af889769b746009f462e6662f3e3eed6
Bruno-Carvalho <brunooakdev@gmail.com>
2021-01-17 11:54:01 -0300

    include serial version in Client entity

--- chapteronejob/src/main/java/com/boakdev/chapteronejob/entities/Client.java
+++ chapteronejob/src/main/java/com/boakdev/chapteronejob/entities/Client.java
@@ -13,6 +13,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_client")
 public class Client implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
