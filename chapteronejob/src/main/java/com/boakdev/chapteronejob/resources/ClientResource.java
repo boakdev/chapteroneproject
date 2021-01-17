a9a90eb816e50a9c480e48233ea10f649356a00e
Bruno-Carvalho <brunooakdev@gmail.com>
2021-01-17 13:59:17 -0300

    ClientResource GET, data seeding

--- chapteronejob/src/main/java/com/boakdev/chapteronejob/resources/ClientResource.java
+++ chapteronejob/src/main/java/com/boakdev/chapteronejob/resources/ClientResource.java
@@ -1,5 +1,38 @@
 package com.boakdev.chapteronejob.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boakdev.chapteronejob.dto.ClientDTO;
+import com.boakdev.chapteronejob.services.ClientService;
+
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientResource {
+
+	@Autowired
+	private ClientService service;
+
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findPerPage(
+
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "2") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> listDto = service.findAllPerPage(pageRequest);
+
+		return ResponseEntity.ok().body(listDto);
+
+	}
 
 }
---
+++ chapteronejob/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruno Carvalho', '000111222', 1000.0, TIMESTAMP WITH TIME ZONE '2021-01-17T13:50:00Z', 2);
